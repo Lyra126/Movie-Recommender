3ce53afcd64fba3521b7e4985d1b27ce771ae26d
Clarissa Cheung <55814603+lynae126@users.noreply.github.com>
2023-10-22 12:13:06 -0400

    adding user object and tasks that need to be done

--- main.cpp
+++ main.cpp
@@ -7,5 +7,22 @@
 using namespace std;
 
 int main(){
+    //parse json files from OMDb API and make user object
+    //Using the Web UI to get user preferences and storing those in a map?
+    string startYear;
+    string endYear;
+    string rating;
+    string runtime;
+    vector<string> genres;
+    vector<string> languages;
+    User user(startYear, endYear, rating, runtime, genres, languages);
+
+    //implement OMDb Web API and get 1,000 data points (movies)
+
+    //store movie data in sets
+
+    //rank movies according to preferences and store rankings in priority queue
+
+    //output json files and put them in a database so the web UI and get them and display them
 
 }
